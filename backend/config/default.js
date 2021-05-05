c46f94a0d711d2444e3828ce4e06cd034a54f1a6
XiiinSheng <57552802+XiiinSheng@users.noreply.github.com>
2021-05-04 19:15:14 -1000

    Delete backend directory

--- backend/config/default.js
+++
@@ -1,8 +0,0 @@
-/**
- * default configs
- */
-
-module.exports = {
-    //put your db key here
-    mongodb_key : 'mongodb+srv://public:public@cluster0.psure.mongodb.net/housingWebsite?retryWrites=true&w=majority'
-}
